9e0277a8b529339914e90450051ad4706f6ec848
Andri Yansyah <andriyansyah58@gmail.com>
2022-11-21 15:57:59 +0700

    update20nov2022

--- src/test/java/com/juaracoding/cucumber/step_definitions/RunnerTest.java
+++ src/test/java/com/juaracoding/cucumber/step_definitions/RunnerTest.java
@@ -4,11 +4,12 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(tags = "",
-        features = {"src/main/resources/Feature/01.Login.feature",
-               // "src/main/resources/Feature/02.Testimonial.feature",
-                //"src/main/resources/Feature/02.TestimonialEdit.feature",
-               "src/main/resources/Feature/07.ContactMassage.feature"},
-               //"src/main/resources/Feature/07.MenuContactmassages.feature",
+        features = {"src/main/resources/Feature/01.URL.feature",
+                "src/main/resources/Feature/01.Login.feature",
+               // "src/main/resources/Feature/03.Testimonial.feature",
+                //"src/main/resources/Feature/03.TestimonialEdit.feature",
+               "src/main/resources/Feature/08.ContactMassage.feature"},
+               //"src/main/resources/Feature/08.MenuContactmassages.feature",
 
         glue = "com.juaracoding.cucumber.step_definitions",
         plugin = {"pretty","html:target/cucumber-reports.html","json:target/cucumber.json"})
